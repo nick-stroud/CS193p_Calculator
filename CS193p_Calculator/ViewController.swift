1f3b9c6a108641d905ba1b3c8d3d53a16766dd13
ov141d <nicholas.stroud@gmail.com>
2016-10-21 18:50:11 -0700

    added buttons and display

--- CS193p_Calculator/ViewController.swift
+++ CS193p_Calculator/ViewController.swift
@@ -9,17 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTypingNumber = false
+    
+    
+    @IBAction func digitPressed(_ sender: UIButton) {
+        if userIsInTheMiddleOfTypingNumber{
+        let digitPressed=sender.currentTitle!
+        display.text = display.text! + digitPressed
+        } else {
+            display.text = sender.currentTitle
+        }
+        userIsInTheMiddleOfTypingNumber = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
